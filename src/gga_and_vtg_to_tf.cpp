37282e1c07a067d8f7370564c5bb94af0c34af90
Geoff Viola <geoffrey.viola@asirobots.com>
2016-06-11 23:08:10 -0600

    removed another raw_

--- src/gga_and_vtg_to_tf.cpp
+++ src/gga_and_vtg_to_tf.cpp
@@ -5,8 +5,8 @@
 #include <tf/transform_broadcaster.h>
 #include <ros/callback_queue.h>
 #include <tf/transform_listener.h>
-#include "raw_nmea/gga.h"
-#include "raw_nmea/vtg.h"
+#include "nmea/gga.h"
+#include "nmea/vtg.h"
 #include "utilities.hpp"
 #include "local_pose_2_5_d.hpp"
 #include "origin_acquirer_gga.hpp"
@@ -31,7 +31,7 @@ public:
         get_transform(GNSS_frame_ID, this->outputTFFrameName);
   }
 
-  inline void GgaCallback(raw_nmea::gga const &message)
+  inline void GgaCallback(nmea::gga const &message)
   {
     auto const xyz =
         get_xyz(this->origin.latitudeDeg, this->origin.longitudeDeg,
@@ -44,7 +44,7 @@ public:
       this->state.z = get<2>(xyz);
     }
 
-    if (raw_nmea::gga::INVALID != message.fix_quality)
+    if (nmea::gga::INVALID != message.fix_quality)
     {
       LocalPose2_5D const local_pose(this->GetState());
       auto const world_to_gnss_transform = get_transform(
@@ -60,7 +60,7 @@ public:
     }
   }
 
-  inline void VtgCallback(raw_nmea::vtg const &message)
+  inline void VtgCallback(nmea::vtg const &message)
   {
     this->vtgTrueTrackMadeGood = message.true_track_made_good;
   }
